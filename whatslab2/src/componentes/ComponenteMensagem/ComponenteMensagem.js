3e54b3ca2eeba4a762ab8abd4f581bb5d6502fe1
Bruno <silva.bruno@outlook.com>
2020-12-19 06:16:22 -0300

    Atualizando a minha branch com a da Monica

---
+++ whatslab2/src/componentes/ComponenteMensagem/ComponenteMensagem.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default class ComponenteMensagem extends React.Component {
+  state = {
+    //---------------------------------------------------------
+    // Array de usuario e mensagens
+    mensagens: [],
+    //---------------------------------------------------------
+    //Valores de estado dos inputs
+    valorInputUsuario: "",
+    valorInputMensagem: ""
+  };
+  
+  render() {
+    const componentesMensagens = this.state.mensagens.map((mensagem) => {
+      return (
+        <p>
+          {mensagem.usuario}: {mensagem.textoMensagem}
+        </p>
+      );
+    });
+    return <div className="App">{componentesMensagens}</div>;
+  }
+}
